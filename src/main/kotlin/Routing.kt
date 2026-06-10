da8b73111f6cb60ca4095fe292bf98fe3a121879
Adam Mathlay <mathlayadam@icloud.com>
2026-06-10 21:16:42 +0200

    Maybe, async

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,24 +1,46 @@
 package io.cuttlefish
 
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
+import io.netty.handler.codec.compression.Zstd.cause
+import kotlinx.coroutines.job
+import java.util.Collections.synchronizedSet
+
+val activeConnections = synchronizedSet(LinkedHashSet<DefaultWebSocketServerSession>())
+
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Server is up and running!")
-        }
-
         webSocket("/chat") {
+            activeConnections += this@webSocket
+            println("New User! Total users: ${activeConnections.size}")
             sendSerialized(ChatMessage("Server", "Welcome to the chat!"))
+
+
+
+            coroutineContext.job.invokeOnCompletion {
+                activeConnections.remove(this@webSocket)
+                if (cause() == null) {
+                    println("Clean disconnect")
+                } else {
+                    println("User disconnected abruptly")
+                }
+                println("Total users remaining: ${activeConnections.size}")
+
+            }
 
             while (true) {
                 val message = receiveDeserialized<ChatMessage>()
                 println("Received from ${message.sender}: ${message.content}")
 
-                sendSerialized(ChatMessage("Server", "I got your message: ${message.content}"))
+                activeConnections.forEach { connection ->
+                    with(connection) {
+                        sendSerialized(ChatMessage("Server", "I got your message: ${message.content}"))
+                    }
+                }
+
+
             }
         }
     }
